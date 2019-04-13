9d9cf0219d8700d98c829fd41e99f4eccf43b7c9
Rong Liu <rliu.pro@gmail.com>
2019-04-12 21:46:05 -0700

    sample udfs

---
+++ src/main/java/com/hive/udf/ArrayMax.java
@@ -0,0 +1,84 @@
+package com.hive.udf;
+
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentLengthException;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentTypeException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ListObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorUtils;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+
+@Description(name = "array_max"
+        , value = "_FUNC_(array) - returns the maximum value of input array."
+        , extended = "Example:\n > select _FUNC_(array) from src;")
+public class ArrayMax extends GenericUDF {
+    private static final int ARG_COUNT = 1; // Number of arguments to this UDF
+    private transient ListObjectInspector arrayOI;
+    private transient ObjectInspector arrayElementOI;
+
+    public ArrayMax() {
+    }
+
+    @Override
+    public ObjectInspector initialize(ObjectInspector[] arguments) throws UDFArgumentException {
+        if (arguments.length != ARG_COUNT) {  // Check if only one argument was passed
+            throw new UDFArgumentLengthException(
+                    "The function array_max(array) takes exactly " + ARG_COUNT + "arguments.");
+        }
+
+        if ("void".equals(arguments[0].getTypeName())) {  // check if input is null
+            return PrimitiveObjectInspectorFactory.javaVoidObjectInspector;
+        }
+
+        if (!arguments[0].getCategory().equals(ObjectInspector.Category.LIST)) { // Check if the argument is of category LIST
+            throw new UDFArgumentTypeException(0,
+                    "\"" + org.apache.hadoop.hive.serde.serdeConstants.LIST_TYPE_NAME + "\" "
+                            + "expected by function array_max, but "
+                            + "\"" + arguments[0].getTypeName() + "\" "
+                            + "is found");
+        }
+
+        arrayOI = (ListObjectInspector) arguments[0];
+        arrayElementOI = arrayOI.getListElementObjectInspector();
+
+        // Check if the comparison is supported for this type
+        if (!ObjectInspectorUtils.compareSupported(arrayElementOI)) {
+            throw new UDFArgumentException("The function array_max"
+                    + " does not support comparison for "
+                    + "\"" + arrayElementOI.getTypeName() + "\""
+                    + " types");
+        }
+
+        return arrayElementOI;
+    }
+
+    @Override
+    public Object evaluate(DeferredObject[] arguments) throws HiveException {
+        Object array = arguments[0].get();
+        if (array == null) {  // Check if array is null
+            return null;
+        }
+
+        int arrayLength = arrayOI.getListLength(array);
+        if (arrayLength <= 0) {  // Check if array is empty
+            return null;
+        }
+
+        Object maxElement = arrayOI.getListElement(array, 0);
+        for (int i = 0; i < arrayLength; i++) {
+            if (ObjectInspectorUtils.compare(maxElement, this.arrayElementOI, arrayOI.getListElement(array, i), this.arrayElementOI) < 0)
+                maxElement = arrayOI.getListElement(array, i);
+        }
+
+        return maxElement;
+    }
+
+    @Override
+    public String getDisplayString(String[] strings) {
+        assert (strings.length == ARG_COUNT);
+        return "array_max(" + strings[0] + ")";
+    }
+}
